24b7c4d2c8d9fcfea8d283a785ba00b8bdf41ea7
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-07-21 14:19:07 -0300

    cart reducer

---
+++ client/src/redux/cartReducer/cart-slice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Produto } from "../../data/Produto";
+
+interface CarrinhoState {
+  carrinho: Produto[];
+}
+
+const initialState: CarrinhoState = {
+  carrinho: JSON.parse(localStorage.getItem("produtos no carrinho") || "[]"),
+};
+
+export const cartSlice = createSlice({
+  initialState,
+  name: "carrinho",
+  reducers: {
+    addProduto: (state, action) => {
+      state.carrinho = [...state.carrinho, action.payload];
+      localStorage.setItem(
+        "produtos no carrinho",
+        JSON.stringify(state.carrinho)
+      );
+    },
+  },
+});
--- client/src/redux/root-reducer.ts
+++ client/src/redux/root-reducer.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import { cartSlice } from "./cartReducer/cart-slice";
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+  cartReducer: cartSlice.reducer,
+});
 
 export type RootReducer = ReturnType<typeof rootReducer>;
