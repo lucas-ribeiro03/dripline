eae8cfcab09712ddfdc64efa3538d18355745597
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-07-28 11:56:25 -0300

    cart component

--- client/src/Components/Modal/Carrinho/Cart.tsx
+++ client/src/Components/Modal/Carrinho/Cart.tsx
@@ -10,9 +10,7 @@ interface CartProps {
 const Cart: React.FC<CartProps> = ({ onClose }) => {
   const [cartItems, setCartItems] = useState<Carrinho[]>([]);
   const getItemsOnCart = () => {
-    const items = JSON.parse(
-      localStorage.getItem("produtos no carrinho") || "[]"
-    );
+    const items = JSON.parse(localStorage.getItem("carrinho") || "[]");
     setCartItems(items);
   };
 
@@ -26,18 +24,44 @@ const Cart: React.FC<CartProps> = ({ onClose }) => {
           <div className={styles.productsOnCart}>
             {cartItems.map((item) => (
               <div className={styles.product}>
-                <div className={styles.itemImage}>
-                  <img src={item.produto_img} alt="imagem do produto" />
+                <div className={styles.itemImage} style={{ gridArea: "box1" }}>
+                  <img
+                    src={`http://${item.produto_img}`}
+                    alt="imagem do produto"
+                    width={80}
+                  />
                 </div>
-                <div className={styles.itemName}>{item.nome}</div>
-                <div className={styles.itemTamanhoRoupa}>
+                <div className={styles.itemName} style={{ gridArea: "box2" }}>
+                  {item.nome}
+                </div>
+                <div
+                  className={styles.itemTamanhoRoupa}
+                  style={{ gridArea: "box3" }}
+                >
                   {item.tamanho_roupa}
                 </div>
-                <div className={styles.itemTamanhoCalcado}>
+
+                <div
+                  className={styles.itemTamanhoCalcado}
+                  style={{ gridArea: "box4" }}
+                >
                   {item.tamanho_tenis}
                 </div>
-                <div className={styles.itemPreco}>{item.preco}</div>
-                <div className={styles.quantidadeItens}>{item.quantidade}</div>
+                <div className={styles.itemPreco} style={{ gridArea: "box5" }}>
+                  R${" "}
+                  {item.preco
+                    .toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })
+                    .replace(".", ",")}
+                </div>
+                <div
+                  className={styles.quantidadeItens}
+                  style={{ gridArea: "box6" }}
+                >
+                  Quantidade: {item.quantidade}
+                </div>
               </div>
             ))}
           </div>
@@ -45,10 +69,14 @@ const Cart: React.FC<CartProps> = ({ onClose }) => {
             <h3>Resumo do pedido</h3>
             <span>
               Total: R${" "}
-              {cartItems.reduce(
-                (acc: number, current: Carrinho) => (acc = current.preco + acc),
-                0
-              )}
+              {cartItems
+                .reduce(
+                  (acc, current: Carrinho) =>
+                    (acc = Number(current.preco) + Number(acc)),
+                  0
+                )
+                .toFixed(2)
+                .replace(".", ",")}
             </span>
           </div>
           <button className={styles.finish}>Finalizar Compra</button>
