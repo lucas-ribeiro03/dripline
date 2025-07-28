9f73ab7e6b1c5550fa026640b636fa369873f31e
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-07-28 10:51:22 -0300

    promocional price

--- client/src/Components/Card/Card.tsx
+++ client/src/Components/Card/Card.tsx
@@ -17,10 +17,12 @@ const Card: React.FC<CardProps> = ({ card, seeProduct }) => {
           <span className={styles.nome}>{card.nome}</span>
         </div>
         <div className={styles.cardPrice}>
-          <span className={styles.promoPreco}>{card.preco_base}</span>
-          <span className={styles.precoBase}>{card.preco_base}</span>
+          <span className={styles.promoPreco}>R$ 99,90 </span>
+          <span className={styles.precoBase}> R$ {card.preco_base}</span>
         </div>
-        <span className={styles.parcela}>ou 12x de {""}</span>
+        <span className={styles.parcela}>
+          ou 12x de {(99.9 / 12).toFixed(2).replace(".", ",")}
+        </span>
         <button onClick={seeProduct}>Ver produto</button>
       </div>
     </div>
