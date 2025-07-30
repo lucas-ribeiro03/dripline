1399e8be98cec09a892b87800968ef289e85dc06
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-07-30 10:10:49 -0300

    removing log

--- server/routes/Anuncios.js
+++ server/routes/Anuncios.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { Anuncio, Produto } = require("../models");
 
 router.get("/:id", async (req, res) => {
-  console.log("EXECUTADO");
   const { id } = req.params;
   const response = await Anuncio.findOne({
     where: { produto_id: id },
